021d0712b1662f9d0aada7206de627b0ff1e5ffb
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2023-11-10 12:11:48 +0530

    setitem for extract

--- Extract Folder/js/script.js
+++ Extract Folder/js/script.js
@@ -1101,27 +1101,29 @@ function question1(data) {
 
 function myFunction1() {
   var x = document.getElementById("myBtn1").value;
-  localStorage["key"] = x;
+  localStorage.setItem("key", x);
+
 }
 
 function myFunction2() {
   var x = document.getElementById("myBtn2").value;
-  localStorage["key"] = x;
+  localStorage.setItem("key", x);
+
 }
 function myFunction3() {
   var x = document.getElementById("myBtn3").value;
+  localStorage.setItem("key", x);
 
-  localStorage["key"] = x;
 }
 function myFunction4() {
   var x = document.getElementById("myBtn4").value;
+  localStorage.setItem("key", x);
 
-  localStorage["key"] = x;
 }
 function myFunction5() {
   var x = document.getElementById("myBtn5").value;
+  localStorage.setItem("key", x);
 
-  localStorage["key"] = x;
 }
 
 function question2(data) {
