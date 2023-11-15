b8d7497830b57e38da3d1a4fd20cca031250a4be
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2023-11-15 11:32:46 +0530

    line 801 added

--- Extract Folder/js/script.js
+++ Extract Folder/js/script.js
@@ -798,6 +798,7 @@ function submitanswer() {
     for (let i = 1; i <= choice_length; i++) {
       var ids = document.getElementById("myBtn" + i).value;
   alert("hii i am id="+ids)
+  alert("localStorage['key']: ", localStorage["key"])
       if (localStorage["key"] == ids) {
         value = localStorage["key"];
         alert("line=803"+value);
